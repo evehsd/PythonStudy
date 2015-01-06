f9e97e2ff7a713ff793a6bd7223a114379781c0c
sdhong <sdhong@evehsdhq-PC.CJI.COM>
2015-01-06 16:05:58 +0900

    study

---
+++ PythonStudy/Study.py
@@ -0,0 +1,13 @@
+
+for data in (2, 45, 55, 200, -100, 99, 37, 10284):
+    if data % 3 ==0:
+        print data
+
+
+
+test = "this is test string"
+test = test[::-1]
+print(test)
+
+
+friends = ['john', 'pat', 'gary', 'michael']
