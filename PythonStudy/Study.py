ce2d6b147a82ed967121a66c0f3cea7f448687f1
sdhong <sdhong@evehsdhq-PC.CJI.COM>
2015-01-07 12:21:34 +0900

    1

--- PythonStudy/Study.py
+++ PythonStudy/Study.py
@@ -1,13 +1,63 @@
-
-for data in (2, 45, 55, 200, -100, 99, 37, 10284):
-    if data % 3 ==0:
-        print data
-
-
-
-test = "this is test string"
-test = test[::-1]
-print(test)
+### 1 ###
+#for data in (2, 45, 55, 200, -100, 99, 37, 10284):
+#    if data % 3 == 0:
+#        print data
 
 
-friends = ['john', 'pat', 'gary', 'michael']
+### 2 ###
+#test = "this is test string"
+#test = test[::-1]
+#print(test)
+
+### 3 ###
+#friends = ['john', 'pat', 'gary', 'michael']
+#num = [1,2,3,4]
+#print"No. %d is %s" % (num[0],friends[0])
+#print"No. %d is %s" % (num[1],friends[1])
+#print"No. %d is %s" % (num[2],friends[2])
+#print"No. %d is %s" % (num[3],friends[3])
+
+
+### 4 ###
+#for x in range(2,10):
+#    for y in range(1,10):
+#        print(x),"X",(y),"=",(x*y)
+
+
+### 5 ###
+
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+### 6 ###
+
+#temperature = input("temperature : ")
+#if temperature >= 72:
+#    print "too hot"
+#else:
+#    print "too cold";
+
+
+### 7 ###
+
+#import datetime
+#today = datetime.datetime.now()
+#print "Today: %s" % today.year,today.month,today.day
+
+
+
+
+
+
+
+
+
+
+
